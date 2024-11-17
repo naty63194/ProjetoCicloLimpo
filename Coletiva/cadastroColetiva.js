51a8247a306498ce66c131069367a733b74ac2a1
NataliaMacedo <nat.vanessa29@gmail.com>
2024-11-17 11:43:12 -0300

    modificações

--- Coletiva/cadastroColetiva.js
+++ Coletiva/cadastroColetiva.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("cadastroColetivaForm");
 
-  // Função de preenchimento automático usando IDs dos campos
   function autoFillForm() {
     document.getElementById("nomeFantasia").value = "Cooperativa Verde";
     document.getElementById("cnpj").value = "12345678000199";
@@ -12,16 +11,13 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("confirmarSenha").value = "senha123";
   }
 
-  // Chama a função de preenchimento automático ao carregar a página
   autoFillForm();
 
   form.addEventListener("submit", function (event) {
     event.preventDefault();
-    // Redireciona para a página HTML de sucesso
     window.location.href = "http://127.0.0.1:3000/Coletiva/cadastroADM.html";
   });
 
-  // Função para adicionar um novo campo de telefone e esconder o botão "+"
   function addPhoneField() {
     const phoneContainer = document.querySelector(".phone-container");
 
@@ -33,10 +29,8 @@ document.addEventListener("DOMContentLoaded", function () {
     
     phoneContainer.appendChild(newPhoneField);
 
-    // Esconde o botão após adicionar o campo
     document.querySelector(".add-phone").style.display = "none";
   }
 
-  // Expõe a função para o botão "+"
   window.addPhoneField = addPhoneField;
 });
